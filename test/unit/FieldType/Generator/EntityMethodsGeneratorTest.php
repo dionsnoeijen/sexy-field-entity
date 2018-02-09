71d78882c44c2822ccc27a855c20b4901c9348b0
Marieke Bednarczyk <mbed@live.nl>
2018-02-09 09:43:38 +0100

    Make sure properties can be typehinted as nullable
    The return type of the getters is always set to nullable because the application seems to need this to display an empty form

--- test/unit/FieldType/Generator/EntityMethodsGeneratorTest.php
+++ test/unit/FieldType/Generator/EntityMethodsGeneratorTest.php
@@ -18,15 +18,81 @@ final class EntityMethodsGeneratorTest extends TestCase
      * @test
      * @covers ::generate
      */
-    public function it_should_generate()
+    public function it_should_generate_when_property_is_not_nullable()
     {
         $body = <<<'EOT'
-public function get{{ methodName }}(): ?string
+public function get{{ methodName }}(): ?\DateTime
 {
     return $this->{{ propertyName }};
 }
 
-public function set{{ methodName }}(string ${{ propertyName }}): {{ section }}
+public function set{{ methodName }}({{ nullable }}\DateTime ${{ propertyName }}): {{ section }}
+{
+    $this->{{ propertyName }} = ${{ propertyName }};
+    return $this;
+}
+
+EOT;
+
+        $structure = [
+            'GeneratorTemplate' => [
+                'entity.methods.php.template' => $body
+            ]
+        ];
+        vfsStream::setup('root', null, $structure);
+
+        $templateDir = TemplateDir::fromString('vfs://root');
+
+        $config = [
+            'field' => [
+                'name' => 'foo',
+                'handle' => 'bar',
+                'generator' => [
+                    'entity' => [
+                        'validator' => [
+                            'NotBlank' => null
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $templateString = <<<'EOT'
+public function getBar(): ?\DateTime
+{
+    return $this->bar;
+}
+
+public function setBar(\DateTime $bar): {{ section }}
+{
+    $this->bar = $bar;
+    return $this;
+}
+
+EOT;
+
+        $field = new Field();
+        $field->setConfig($config);
+
+        $result = EntityMethodsGenerator::generate($field, $templateDir);
+        $expected = Template::create($templateString);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     * @covers ::generate
+     */
+    public function it_should_generate_when_property_is_nullable()
+    {
+        $body = <<<'EOT'
+public function get{{ methodName }}(): ?\DateTime
+{
+    return $this->{{ propertyName }};
+}
+
+public function set{{ methodName }}({{ nullable }}\DateTime ${{ propertyName }}): {{ section }}
 {
     $this->{{ propertyName }} = ${{ propertyName }};
     return $this;
@@ -51,12 +117,80 @@ EOT;
         ];
 
         $templateString = <<<'EOT'
-public function getBar(): ?string
+public function getBar(): ?\DateTime
 {
     return $this->bar;
 }
 
-public function setBar(string $bar): {{ section }}
+public function setBar(?\DateTime $bar): {{ section }}
+{
+    $this->bar = $bar;
+    return $this;
+}
+
+EOT;
+
+        $field = new Field();
+        $field->setConfig($config);
+
+        $result = EntityMethodsGenerator::generate($field, $templateDir);
+        $expected = Template::create($templateString);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     * @covers ::generate
+     */
+    public function it_should_generate_when_property_is_nullable_and_generator_has_other_config()
+    {
+        $body = <<<'EOT'
+public function get{{ methodName }}(): ?\DateTime
+{
+    return $this->{{ propertyName }};
+}
+
+public function set{{ methodName }}({{ nullable }}\DateTime ${{ propertyName }}): {{ section }}
+{
+    $this->{{ propertyName }} = ${{ propertyName }};
+    return $this;
+}
+
+EOT;
+
+        $structure = [
+            'GeneratorTemplate' => [
+                'entity.methods.php.template' => $body
+            ]
+        ];
+        vfsStream::setup('root', null, $structure);
+
+        $templateDir = TemplateDir::fromString('vfs://root');
+
+        $config = [
+            'field' => [
+                'name' => 'foo',
+                'handle' => 'bar',
+                'generator' => [
+                    'entity' => [
+                        'validator' => [
+                            'Length' => [
+                                'max' => 255
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $templateString = <<<'EOT'
+public function getBar(): ?\DateTime
+{
+    return $this->bar;
+}
+
+public function setBar(?\DateTime $bar): {{ section }}
 {
     $this->bar = $bar;
     return $this;
--- test/unit/FieldType/Generator/EntityPropertiesGeneratorTest.php
+++ test/unit/FieldType/Generator/EntityPropertiesGeneratorTest.php
@@ -18,10 +18,60 @@ final class EntityPropertiesGeneratorTest extends TestCase
      * @test
      * @covers ::generate
      */
-    public function it_should_generate()
+    public function it_should_generate_when_property_is_not_nullable()
     {
         $body = <<<'EOT'
+/** @var {{ nullable }}\DateTime */
+protected ${{ propertyName }};
+
+EOT;
+
+        $structure = [
+            'GeneratorTemplate' => [
+                'entity.properties.php.template' => $body
+            ]
+        ];
+        vfsStream::setup('root', null, $structure);
+
+        $templateDir = TemplateDir::fromString('vfs://root');
+
+        $config = [
+            'field' => [
+                'name' => 'foo',
+                'handle' => 'bar',
+                'generator' => [
+                    'entity' => [
+                        'validator' => [
+                            'NotBlank' => null
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $templateString = <<<'EOT'
 /** @var \DateTime */
+protected $bar;
+
+EOT;
+
+        $field = new Field();
+        $field->setConfig($config);
+
+        $result = EntityPropertiesGenerator::generate($field, $templateDir);
+        $expected = Template::create($templateString);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     * @covers ::generate
+     */
+    public function it_should_generate_when_property_is_nullable()
+    {
+        $body = <<<'EOT'
+/** @var {{ nullable }}\DateTime */
 protected ${{ propertyName }};
 
 EOT;
@@ -43,7 +93,59 @@ EOT;
         ];
 
         $templateString = <<<'EOT'
-/** @var \DateTime */
+/** @var ?\DateTime */
+protected $bar;
+
+EOT;
+
+        $field = new Field();
+        $field->setConfig($config);
+
+        $result = EntityPropertiesGenerator::generate($field, $templateDir);
+        $expected = Template::create($templateString);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     * @covers ::generate
+     */
+    public function it_should_generate_when_property_is_nullable_and_generator_has_other_config()
+    {
+        $body = <<<'EOT'
+/** @var {{ nullable }}\DateTime */
+protected ${{ propertyName }};
+
+EOT;
+
+        $structure = [
+            'GeneratorTemplate' => [
+                'entity.properties.php.template' => $body
+            ]
+        ];
+        vfsStream::setup('root', null, $structure);
+
+        $templateDir = TemplateDir::fromString('vfs://root');
+
+        $config = [
+            'field' => [
+                'name' => 'foo',
+                'handle' => 'bar',
+                'generator' => [
+                    'entity' => [
+                        'validator' => [
+                            'Length' => [
+                                'max' => 255
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $templateString = <<<'EOT'
+/** @var ?\DateTime */
 protected $bar;
 
 EOT;
